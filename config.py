077d1b175a09cfe183feff432acfadec585360d8
Zaid _ <162994967+cv0v@users.noreply.github.com>
2024-03-17 16:14:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv("API_ID", "13251350"))
 API_HASH = os.getenv("API_HASH", "66c0eacb36f9979ae6d153f207565cd6")
-CHAT_ID = -1001635295976
+CHAT_ID = int(os.getenv("CHAT_ID", "1"))
 CHANNEL_USERNAMWE = os.getenv("CHANNEL_USERNAMWE", "")
 SPECIFIC_READER = os.getenv("SPECIFIC_READER", "") # The reader name in arabic , ex: فارس عباد , default to random.
 SESSION_STRING = os.getenv("SESSION_STRING", "") # To Extract string_session just run "String.py"
